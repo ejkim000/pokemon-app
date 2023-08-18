25e5143b479ebd51456b9486ae735543f99af53a
ejkim000 <developer.ejk@gmail.com>
2023-08-18 15:10:52 -0400

    update style

--- views/Index.jsx
+++ views/Index.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 
 function Index({ pokemons }) {
-  const myStyle = {
+  const h1Style = {
     color: "#ffffff",
     backgroundColor: "orange",
+    textAlign: "center",
+    margin: "20px",
+    padding: "20px"
+  };
+
+  const ulStyle = {
+    fontSize: "18px",
+    lineHeight: "1.8",
+    textAlign: "center",
+    margin: "20px",
+    padding: "20px",
+    listStyleType: "none"
   };
 
   return (
     <div>
-        <h1 style={myStyle}>See All The Pokemon!</h1>
-        <ul>
+        <h1 style={h1Style}>See All The Pokemon!</h1>
+        <ul style={ulStyle}>
             { pokemons.map((pokemon, i) => {
                 const firstUpper = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
                 return <li key={i}><a href={`pokemon/${i}`}>{firstUpper}</a></li>
--- views/Show.jsx
+++ views/Show.jsx
@@ -1,22 +1,33 @@
 import React from "react";
 
 function Show({ pokemon }) {
-  const myStyle = {
+  const h1Style = {
     color: "#ffffff",
     backgroundColor: "orange",
+    textAlign: "center",
+    margin: "20px",
+    padding: "20px",
   };
 
-  const firstUpper = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
-  const pokemonImg = pokemon.img + '.jpg';
+  const containerStyle = {
+    textAlign: "center",
+    lineHeight: "1.8",
+    margin: "20px",
+    padding: "20px",
+  };
+  const firstUpper =
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const pokemonImg = pokemon.img + ".jpg";
 
   return (
     <div>
-        <h1 style={myStyle}>Gotta Catch 'Em All</h1>
+      <h1 style={h1Style}>Gotta Catch 'Em All</h1>
+      <div style={containerStyle}>
         <h2>{firstUpper}</h2>
         <img src={pokemonImg} alt={firstUpper} /> <br />
         <a href="/pokemon">Back</a>
+      </div>
     </div>
-  
   );
 }
 
